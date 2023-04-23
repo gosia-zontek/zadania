90631e45504850af6d166f988605554c32cf04aa
gosia-zontek <131551826+gosia-zontek@users.noreply.github.com>
2023-04-23 09:48:16 +0200

    dzien1_Zadania Petle

---
+++ src/dzien1/ZadaniaPetle.java
@@ -0,0 +1,22 @@
+package dzien1;
+
+public class ZadaniaPetle {
+    public static void main(String[] args) {
+
+        for (int i = 99; i >0; i=i-2) {
+//            if(i%2==0) {
+
+                System.out.println("wypisz liczby: " + i);
+//            }
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println();
+        }
+//        int number =0;
+//        while (number <10){
+//            System.out.println(number+1);
+//            number++;
+//        }
+    }
+}
