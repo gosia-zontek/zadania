811a3cad8f4a8eae89789e28aad1c7455684fbb2
gosia-zontek <131551826+gosia-zontek@users.noreply.github.com>
2023-04-23 11:33:04 +0200

    zadania Imiona

---
+++ src/dzien1/Imiona.java
@@ -0,0 +1,28 @@
+package dzien1;
+
+public class Imiona {
+    public static void main(String[] args) {
+        //   Stwórz tablicę imion (z powtórzeniami) i następnie policz, ile razy występuje w niej imię "Jan".
+//        Stwórz tablicę imion (bez powtórzeń) i następnie podaj, na którym indexie znajduje się w niej imię "Jan".
+
+        String[] names = new String[]{"Basia", "Olek", "Ania", "Jan", "Pawel", "Jan", "Tomek"};
+        int szukanyIndex = 0;
+        String szukaneImie = "Jan";
+        for (int aktualnyIndex = 0; aktualnyIndex < names.length; aktualnyIndex++) {
+            String aktualneImie = names[aktualnyIndex];
+            if (aktualneImie.equals(szukaneImie)) {
+                szukanyIndex = aktualnyIndex;
+
+                break;
+            }
+//
+//            if (names[aktualnyIndex].equals(szukaneImie)) {
+//                ilerazy++;
+        }
+        System.out.println("szukane imie Jan jest na tym miejscu: " + szukanyIndex);
+
+        //  System.out.println("w tablicy znalazlem imie " + szukaneImie+ " " + ilerazy + " razy");
+        //  System.out.printf("w tablicy znalazlem imie %s %s razy",szukaneImie,ilerazy);
+
+    }
+}
