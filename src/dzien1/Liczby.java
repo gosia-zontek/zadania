1bd23bf8de879c84ccc4b477dd0a68ea9922d2f5
gosia-zontek <131551826+gosia-zontek@users.noreply.github.com>
2023-04-23 11:56:49 +0200

    zadania Liczby

--- src/dzien1/Liczby.java
+++ src/dzien1/Liczby.java
@@ -3,20 +3,21 @@ package dzien1;
 public class Liczby {
     //    Stwórz tablicę liczb całkowitych i następnie znajdź największy i najmniejszy element tej tablicy.
     public static void main(String[] args) {
-        int[] numbers = new int[]{3,5,9, 1, 3, 4, 5, 3};
+        int[] numbers = new int[]{3, 5, 9, 1, 3, 4, 5, 3};
         int max = numbers[0];
-        int min =numbers[0];
+        int min = numbers[0];
         for (int i = 0; i < numbers.length; i++) {
             int aktualnyNumer = numbers[i];
             if (aktualnyNumer > max) {
-                max =aktualnyNumer;
+                max = aktualnyNumer;
             }
             if (aktualnyNumer < min) {
-                min =aktualnyNumer;
+                min = aktualnyNumer;
             }
         }
         System.out.println("max = " + max);
         System.out.println("min = " + min);
+        System.out.println("komentarz");
 
     }
 }
