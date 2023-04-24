33b5115633d8261ecaab75fd1c99934c174fbb01
gosia-zontek <131551826+gosia-zontek@users.noreply.github.com>
2023-04-24 19:08:09 +0200

    zadania roznica

--- src/git/Main.java
+++ src/git/Main.java
@@ -1,17 +1,23 @@
 package git;
 
-import java.sql.SQLOutput;
-
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = new int[]{2,4,5,9,12,323,49};
+        int[] numbers = new int[]{2, 4, 5, 9, 12, 323, 49};
 
 
         int suma = 0;
+        int roznica = 0;
         for (int i = 0; i < numbers.length; i++) {
             System.out.println(numbers[i]);
             suma += numbers[i];
         }
-        System.out.println("suma liczb: "+ suma);
+        System.out.println("suma liczb: " + suma);
+
+        for (int r = 0; r < numbers.length; r++) {
+            roznica -= numbers[r];
+
+        }
+        System.out.println("roznica liczb: " + roznica);
     }
+
 }
