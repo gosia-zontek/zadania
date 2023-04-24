b81e838ba4a50fa61b6ba818fd290f5160ed1e3d
gosia-zontek <131551826+gosia-zontek@users.noreply.github.com>
2023-04-24 19:47:44 +0200

    zadania imiona

---
+++ src/git/Proba.java
@@ -0,0 +1,22 @@
+package git;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Proba {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+
+        names.add("Ala");
+        names.add("Ola");
+        names.add("Ania");
+        names.add("Tomek");
+        names.add("Arek");
+
+        System.out.println("imiona: " + names);
+
+        String x = names.get(3);
+        System.out.println("imie pod tym indexem to: " + x);
+
+    }
+}
