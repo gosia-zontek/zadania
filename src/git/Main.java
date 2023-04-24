4211b864fa47b402938179fd16716bec398de427
Nebrakada <marcin.pawezka@gmail.com>
2023-04-24 18:56:18 +0200

    modify summing numbers method

--- src/git/Main.java
+++ src/git/Main.java
@@ -1,17 +1,14 @@
 package git;
 
-import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = new int[]{2,4,5,9,12,323,49};
-
+        int[] numbers = new int[]{1, 13, 52, 2,4,5,9,12,323,49};
 
         int suma = 0;
         for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
             suma += numbers[i];
         }
-        System.out.println("suma liczb: "+ suma);
+        System.out.println("suma liczb= "+ suma);
     }
 }
