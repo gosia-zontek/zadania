ab872c4b3abf73d1fa418e887835b59b4609acf3
gosia-zontek <131551826+gosia-zontek@users.noreply.github.com>
2023-05-09 18:07:36 +0200

    zadania Obwod_kola

---
+++ src/maj9/Zadanie1.java
@@ -0,0 +1,24 @@
+package maj9;
+
+import java.util.Scanner;
+
+public class Zadanie1 {
+    public static void main(String[] args) {
+        int a = 1;
+        //napisz program, ktory pobierze od uzytkownika srednice okregu (zmienna typu float)
+        // i obliczy obwod tego okregu. do obliczen przyjmij najpierw pi = 3.14
+        //nastepnie skorzystaj z wbudowanej klasy Math i znajdujacej sie tam stalej PI
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please give the diameter of the circle: ");
+        Float diameter = s.nextFloat();
+        double pi = 3.14;
+        Float circumference1 = (float) (diameter * pi);
+        Float circumference = (float) (2.0 * Math.PI * (diameter/2.0));
+        String input = s.nextLine();
+
+        System.out.println("Obwod to " + circumference);
+        System.out.println("Obwod to " + circumference1);
+
+    }
+}
