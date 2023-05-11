bfc87ad4e36a6ac08e2ef0085f5cc30f89230113
gosia-zontek <131551826+gosia-zontek@users.noreply.github.com>
2023-05-11 20:20:28 +0200

    zadania Zadanie6_sumaciag

---
+++ src/maj_11/Zadanie_6.java
@@ -0,0 +1,29 @@
+package maj_11;
+
+import java.util.Scanner;
+
+public class Zadanie_6 {
+    public static void main(String[] args) {
+        //napisz program, ktory pobierze od uzytkownika liczbe n typu int i obliczy sume szeregu harmonicznego od 1 do n, zgodnie ze wzorem
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("podaj liczbe: ");
+        int number = scanner.nextInt();
+
+        float czesc;
+        float wynik = 0;
+        for (int i = 1; i <= number; i++) {
+           // czesc = 1f/(i);
+             czesc = (float) (1.0/(i));
+            System.out.println(String.format("%.2f", czesc));
+           wynik += 1/i;
+            System.out.println(wynik);
+
+
+        }
+
+    }
+
+}
+
+
