96dfe6c36ff9570b14188489c8164993a7e02698
gosia-zontek <131551826+gosia-zontek@users.noreply.github.com>
2023-05-16 18:13:10 +0200

    Zadanie_12_spacje

--- src/git/Proba.java
+++ src/git/Proba.java
@@ -1,27 +1,27 @@
-package git;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Proba {
-    public static void main(String[] args) {
-        List<String> names = new ArrayList<>();
-
-        names.add("Ala");
-        names.add("Ola");
-        names.add("Ania");
-        names.add("Tomek");
-        names.add("Arek");
-
-        System.out.println("imiona: " + names);
-
-        String x = names.get(3);
-        System.out.println("imie pod tym indexem to: " + x);
-
-        names.addAll(List.of("Maciek", "Adam"));
-
-        int maciek = names.indexOf("Maciek");
-        System.out.println("Maciek jest pod indexem: " + maciek);
-
-    }
-}
+//package git;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class Proba {
+//    public static void main(String[] args) {
+//        List<String> names = new ArrayList<>();
+//
+//        names.add("Ala");
+//        names.add("Ola");
+//        names.add("Ania");
+//        names.add("Tomek");
+//        names.add("Arek");
+//
+//        System.out.println("imiona: " + names);
+//
+//        String x = names.get(3);
+//        System.out.println("imie pod tym indexem to: " + x);
+//
+//        names.addAll(List.of("Maciek", "Adam"));
+//
+//        int maciek = names.indexOf("Maciek");
+//        System.out.println("Maciek jest pod indexem: " + maciek);
+//
+//    }
+//}
