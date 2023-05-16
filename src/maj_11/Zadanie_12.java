7170f247c919847bf2eb7ac9d8be2fa2c1172792
gosia-zontek <131551826+gosia-zontek@users.noreply.github.com>
2023-05-16 18:12:34 +0200

    Zadanie_12_spacje

---
+++ src/maj_11/Zadanie_12.java
@@ -0,0 +1,34 @@
+package maj_11;
+
+import java.util.Scanner;
+
+public class Zadanie_12 {
+    public static void main(String[] args) {
+        String text = getUserText();
+        String textWithoutSpaces = getTextWithoutSpaces(text);
+        double allCharacters = text.length();
+        double withoutSpaces = textWithoutSpaces.length();
+        double result = getResult(allCharacters, withoutSpaces);
+        System.out.println(text.length());
+        System.out.println(textWithoutSpaces.length());
+        System.out.printf("Procent spacji w tekście wynosi: %5.2f %%", result);
+    }
+
+    public static String getUserText() {
+        System.out.println("Please write a text:");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public static String getTextWithoutSpaces(String text) {
+        String textWithoutSpaces = "";
+        textWithoutSpaces = text.replaceAll(" ", "");
+        return textWithoutSpaces;
+    }
+
+    public static Double getResult(double allCharacters, double withoutSpaces) {
+        Double result = 0.0;
+        result = ((allCharacters-withoutSpaces)/allCharacters)*100;
+        return result;
+    }
+}
