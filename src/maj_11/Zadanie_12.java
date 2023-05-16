a4d4e0d697b8c56b7fd05f20a76ac1984b70ea08
gosia-zontek <131551826+gosia-zontek@users.noreply.github.com>
2023-05-16 18:00:04 +0200

    All

---
+++ src/maj_11/Zadanie_12.java
@@ -0,0 +1,15 @@
+package maj_11;
+
+import java.util.Scanner;
+
+public class Zadanie_12 {
+    public static void main(String[] args) {
+        //Napisz program, ktory pobierze od uzytkownika tekst (zmienna String)
+        //i policzy jakim procentem wszystkich znakow tekstu byl znak spacji
+        //czyli liczba spacji/liczba wszystkich znakow *100%
+
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+
+    }
+}
